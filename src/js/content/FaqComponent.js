e33cd2a196e89a703e151c4bc1a046b7e52fa699
Gulu <bponnaluri@gmail.com>
2026-01-09 15:57:06 -0500

    Updates

--- src/js/content/FaqComponent.js
+++ src/js/content/FaqComponent.js
@@ -15,6 +15,11 @@ export class FaqComponent extends BaseTemplateComponent {
     return `
       <details>
         <summary>FAQ</summary>
+    
+        <h3>How can I report a bug or request a new feature </h3>
+    
+        <p>Email gulu@createthirdplaces.com, or create an issue 
+        <a href=https://codeberg.org/createthirdplaces/frontend/issues>here</a></p>
         <h3>Why is JavaScript being used to render static content?</h3>
         <p>The main reason is to place content inside shadow DOM to make it more difficult for bots to parse. Automated
          traffic is not authorized to access createthirdplaces.com, and the shadow DOM is a backup measure if bots 
@@ -30,8 +35,8 @@ export class FaqComponent extends BaseTemplateComponent {
          <p>
          If you notice any issues using createthirdplaces.com with assistive
           technology, email gulu@createthirdplaces.com or create a bug report 
-          <a href="https://codeberg.org/createthirdplaces/frontend">here.</a> Code contributions to improve accessibility
-           in the form of a pull request are also welcome.</p>
+          <a href="https://codeberg.org/createthirdplaces/frontend/issues">here.</a> Code contributions to improve accessibility
+           in the form of a <a href="https://codeberg.org/createthirdplaces/frontend/pulls">pull request</a> are also welcome.</p>
          
         <h3>Why does Create Third Places not have a social media presence?</h3>
         
