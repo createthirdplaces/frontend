3ea13528f81b19aa8dcb85d22035c8fb8ee5936c
Bharat Ponnaluri <gulu@createthirdplaces.com>
2026-06-14 00:48:01 -0400

    Bug fixes

--- src/js/globalProd.js
+++ src/js/globalProd.js
@@ -4,7 +4,7 @@ import {blockJavaScriptFunctions} from "/js/bots/block.js"
 export function runGlobal(){
   console.log("Hi");
   if(window.location.href.includes("createthirdplaces.com")){
-    window.location.href = "createthirdplaces.org";
+    window.location.href = "https://createthirdplaces.org/";
   }
   blockJavaScriptFunctions();
 }
