7218a717efa2c4e180b1d68e3650d920027ffd55
Bharat Ponnaluri <gulu@createthirdplaces.com>
2026-07-05 01:02:04 -0400

    Remove console log

--- src/js/globalProd.js
+++ src/js/globalProd.js
@@ -1,8 +1,6 @@
 import {blockJavaScriptFunctions} from "/js/bots/block.js"
 
-
 export function runGlobal(){
-  console.log("Hi");
   if(window.location.href.includes("createthirdplaces.com")){
     window.location.href = "https://createthirdplaces.org/";
   }
