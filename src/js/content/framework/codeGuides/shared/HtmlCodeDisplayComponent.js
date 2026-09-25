53e1365e0fbe304f99fa3e354a2aa1ce25f24f9d
Bharat Ponnaluri <gulu@createthirdplaces.com>
2026-09-25 02:34:27 -0400

    Update HtmlCodeDisplayComponent.js to remove extra indentation and update getting started page

--- src/js/content/framework/codeGuides/shared/HtmlCodeDisplayComponent.js
+++ src/js/content/framework/codeGuides/shared/HtmlCodeDisplayComponent.js
@@ -3,7 +3,7 @@ import {BaseTemplateComponent} from "../../../../shared/lib/places-js-latest.js"
 
 export class HtmlCodeDisplayComponent extends BaseTemplateComponent {
   getTemplateStyle(){
-      return `
+    return `
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/styles/default.min.css">
 
       <style>
@@ -15,8 +15,27 @@ export class HtmlCodeDisplayComponent extends BaseTemplateComponent {
   }
 
   render(){
-    const code = this.getCode().replaceAll("&gt;",">")
+    let code = this.getCode().replaceAll("&gt;",">")
       .replaceAll("&lt;","<");
+    
+    let indent = 0;
+   
+    while(true){
+      const charCode = code.charCodeAt(indent);
+      if(charCode !== 10 && charCode !== 32){
+        break;
+      }
+      indent++; 
+    }
+    indent--;
+
+    const lines = code.split("\n");
+    for(let i=0; i<lines.length; i++){
+      lines[i] = lines[i].substring(indent); 
+    }
+    
+    code = lines.join("\n");
+    
     return `
     <code>
       <pre>${hljs.highlightAuto(code).value}</pre>
